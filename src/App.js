0719db358456dd0b4959f205466887b2a7a5e37c
d wu <dwu@ds-MacBook-Pro.local>
2020-08-22 22:50:13 -0500

    adding initial header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
 import database from "./firebase";
+import Header from "./Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
