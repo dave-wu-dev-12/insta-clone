960a352fa64dda2888417cb50682b91fc730bc27
d wu <dwu@ds-MacBook-Pro.local>
2020-08-22 23:25:03 -0500

    adding post

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import database from "./firebase";
 import Header from "./Header";
+import Post from "./Post";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Post />
     </div>
   );
 }
