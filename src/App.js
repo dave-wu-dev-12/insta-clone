ac2a601e2b5199c3d963a9e707f01a9dceb67393
d wu <dwu@ds-MacBook-Pro.local>
2020-08-23 21:58:28 -0500

    adding posts with firestore

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import database from "./firebase";
 import Header from "./Header";
 import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    database
+      .collection("posts")
+      .onSnapshot((snapshot) =>
+        setPosts(snapshot.docs.map((doc) => ({ id: doc.id, post: doc.data() })))
+      );
+
+    return () => {};
+  }, []);
+
   return (
     <div className="App">
       <Header />
-      <Post />
+      {posts.map(({ id, post }) => (
+        <Post
+          key={id}
+          avatarSrc={post.avatarSrc}
+          userName={post.userName}
+          location={post.location}
+          imageUrl={post.imageUrl}
+          caption={post.caption}
+        />
+      ))}
     </div>
   );
 }
